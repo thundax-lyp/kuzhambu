6a3e905b426420039f59cb806ab1c553ae9cfd29
Thundax <thundax@qq.com>
2026-05-27 00:14:31 +0800

    Feat(common-cache): 迁移缓存基础能力

---
+++ kuzhambu-servers/common/kuzhambu-common-cache/src/main/java/com/thundax/kuzhambu/common/cache/KuzhambuCacheNames.java
@@ -0,0 +1,8 @@
+package com.thundax.kuzhambu.common.cache;
+
+public final class KuzhambuCacheNames {
+
+    public static final String PREFIX = "_KUZHAMBU_";
+
+    private KuzhambuCacheNames() {}
+}
