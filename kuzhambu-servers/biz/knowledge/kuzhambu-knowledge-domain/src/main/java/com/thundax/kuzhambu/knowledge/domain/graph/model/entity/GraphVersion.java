bd345a66dd3b78bd6bb540cdf878a2927daa2f0f
thundax <thundax@qq.com>
2026-06-23 13:46:20 +0800

    Refactor(knowledge): 重定义图谱版本领域契约

--- kuzhambu-servers/biz/knowledge/kuzhambu-knowledge-domain/src/main/java/com/thundax/kuzhambu/knowledge/domain/graph/model/entity/GraphVersion.java
+++ kuzhambu-servers/biz/knowledge/kuzhambu-knowledge-domain/src/main/java/com/thundax/kuzhambu/knowledge/domain/graph/model/entity/GraphVersion.java
@@ -2,17 +2,23 @@ package com.thundax.kuzhambu.knowledge.domain.graph.model.entity;
 
 import com.thundax.kuzhambu.knowledge.domain.graph.model.valueobject.GraphExtractionTaskId;
 import java.util.Date;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GraphVersion {
     private Long id;
     private Long versionId;
     private GraphExtractionTaskId taskId;
     private Long candidateId;
     private String taskType;
+    private String scopeType;
+    private String scopeJson;
     private String sourceContentType;
     private Long sourceContentId;
     private Integer versionNo;
