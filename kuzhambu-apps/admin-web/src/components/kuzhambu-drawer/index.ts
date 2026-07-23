cdbee75351c61aaa4c609c83eaea429d1bf02e3d
Thundax <thundax@qq.com>
2026-07-23 14:39:47 +0800

    Refactor(admin-web): 约束抽屉底部操作结构

--- kuzhambu-apps/admin-web/src/components/kuzhambu-drawer/index.ts
+++ kuzhambu-apps/admin-web/src/components/kuzhambu-drawer/index.ts
@@ -1,2 +1,6 @@
 export { KuzhambuDrawer } from "./kuzhambu-drawer";
-export type { KuzhambuDrawerProps, KuzhambuDrawerSize } from "./kuzhambu-drawer";
+export type {
+    KuzhambuDrawerFooterAction,
+    KuzhambuDrawerProps,
+    KuzhambuDrawerSize
+} from "./kuzhambu-drawer";
