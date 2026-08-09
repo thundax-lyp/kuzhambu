f2174d1c710b4d294b1d21b011cb1a86bc3c3201
Thundax <thundax@qq.com>
2026-08-09 21:07:08 +0800

    Test(servers): 全域校验REST接口硬规则

---
+++ kuzhambu-servers/common/kuzhambu-common-test/src/test/java/com/thundax/kuzhambu/common/test/architecture/RepositoryApiHardRulesArchitectureTest.java
@@ -0,0 +1,23 @@
+package com.thundax.kuzhambu.common.test.architecture;
+
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class RepositoryApiHardRulesArchitectureTest {
+
+    private static final Path BUSINESS_SOURCE_ROOT = Path.of("../../biz");
+
+    @Test
+    void apiSourceShouldKeepHardRuleContract() throws Exception {
+        ApiAnnotationArchitectureRuleSupport.assertOperationDeclaresAccessAnnotation(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertRestControllersDeclareRequestMapping(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertRestControllersDeclareApi(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertApiTagsDoNotUseNumericPrefix(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertMappedMethodsDeclareOperation(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertMappedMethodsDeclareSingleHttpMapping(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertMappedMethodsUsePostOrGetMapping(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertJsonRequestMethodsUsePostMapping(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertRequestBodyRequestParametersDeclareValid(BUSINESS_SOURCE_ROOT);
+        ApiAnnotationArchitectureRuleSupport.assertControllersDoNotCreateResponses(BUSINESS_SOURCE_ROOT);
+    }
+}
