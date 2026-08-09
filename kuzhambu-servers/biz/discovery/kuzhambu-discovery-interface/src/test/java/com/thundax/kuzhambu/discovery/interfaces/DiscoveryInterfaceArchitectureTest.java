ce167f7e6c036bbe212582c004ab8bec78b1a9ab
Thundax <thundax@qq.com>
2026-08-09 22:30:53 +0800

    Test(discovery): 接入REST动作命名门禁

--- kuzhambu-servers/biz/discovery/kuzhambu-discovery-interface/src/test/java/com/thundax/kuzhambu/discovery/interfaces/DiscoveryInterfaceArchitectureTest.java
+++ kuzhambu-servers/biz/discovery/kuzhambu-discovery-interface/src/test/java/com/thundax/kuzhambu/discovery/interfaces/DiscoveryInterfaceArchitectureTest.java
@@ -1,12 +1,15 @@
 package com.thundax.kuzhambu.discovery.interfaces;
 
+import com.thundax.kuzhambu.common.test.architecture.ApiAnnotationArchitectureRuleSupport;
 import com.thundax.kuzhambu.common.test.architecture.ApiSurfaceArchitectureRuleSupport;
+import com.thundax.kuzhambu.common.test.architecture.ArchitectureRuleAllowance;
 import com.thundax.kuzhambu.common.test.architecture.BoundaryAssemblerNullnessAllowances;
 import com.thundax.kuzhambu.common.test.architecture.ConcurrencyArchitectureRuleSupport;
 import com.thundax.kuzhambu.common.test.architecture.NamingArchitectureRuleSupport;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class DiscoveryInterfaceArchitectureTest {
@@ -18,6 +21,8 @@ class DiscoveryInterfaceArchitectureTest {
         JavaClasses classes = ConcurrencyArchitectureRuleSupport.importMainClasses(BASE_PACKAGE + ".interfaces");
         ConcurrencyArchitectureRuleSupport.shouldNotUseCompletableFutureAsyncWithoutExecutor(BASE_PACKAGE)
                 .check(classes);
+        ApiAnnotationArchitectureRuleSupport.assertControllerActionsUseVerbWhitelist(
+                Path.of("src/main/java"), legacyActionVerbAllowances());
         ApiSurfaceArchitectureRuleSupport.assertApiModelsDoNotExposePriority(Path.of("src/main/java"));
         ApiSurfaceArchitectureRuleSupport.assertSortRequestsUseOrderedIdsOnly(Path.of("src/main/java"));
         NamingArchitectureRuleSupport.assertBoundaryAssemblerPublicMethodsUseNonNullContracts(
@@ -27,5 +32,22 @@ class DiscoveryInterfaceArchitectureTest {
                         "com.thundax.kuzhambu.discovery.interfaces.admin.search.assembler.DiscoverySearchStatisticsInterfaceAssembler",
                         "com.thundax.kuzhambu.discovery.interfaces.portal.qa.assembler.DiscoveryQaPortalInterfaceAssembler",
                         "com.thundax.kuzhambu.discovery.interfaces.portal.search.assembler.DiscoverySearchPortalInterfaceAssembler"));
+    }
+
+    private static List<ArchitectureRuleAllowance> legacyActionVerbAllowances() {
+        return List.of(
+                actionVerbAllowance("DiscoveryQaPortalStreamController"),
+                actionVerbAllowance("DiscoveryQaPortalController"),
+                actionVerbAllowance("DiscoveryQaConversationStreamController"),
+                actionVerbAllowance("DiscoveryQaAdminController"),
+                actionVerbAllowance("DiscoveryQaConversationController"),
+                actionVerbAllowance("DiscoverySearchStatisticsController"));
+    }
+
+    private static ArchitectureRuleAllowance actionVerbAllowance(String controller) {
+        return ArchitectureRuleAllowance.of(
+                "CONTROLLER_ACTION_VERB:*" + controller + ".java*",
+                "Discovery controller retains legacy action names or paths outside the shared verb whitelist.",
+                "Rename the controller method and action path with a shared verb, update callers, then remove this allowance.");
     }
 }
