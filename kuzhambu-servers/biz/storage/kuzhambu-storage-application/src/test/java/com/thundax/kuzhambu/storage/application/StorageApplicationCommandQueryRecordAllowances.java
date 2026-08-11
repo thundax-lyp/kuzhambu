fb26eb6c2d76db9caf2d1a7b12d49b3242537b61
Thundax <thundax@qq.com>
2026-08-11 14:18:40 +0800

    Refactor(storage): 将 ListStorageMimeTypesQuery 改为 record

--- kuzhambu-servers/biz/storage/kuzhambu-storage-application/src/test/java/com/thundax/kuzhambu/storage/application/StorageApplicationCommandQueryRecordAllowances.java
+++ kuzhambu-servers/biz/storage/kuzhambu-storage-application/src/test/java/com/thundax/kuzhambu/storage/application/StorageApplicationCommandQueryRecordAllowances.java
@@ -14,7 +14,6 @@ final class StorageApplicationCommandQueryRecordAllowances {
 
     static List<ArchitectureRuleAllowance> legacyAllowances() {
         return List.of(
-                legacy("COMMAND_QUERY_RECORD:com.thundax.kuzhambu.storage.application.query.ListStorageMimeTypesQuery"),
                 legacy("COMMAND_QUERY_RECORD:com.thundax.kuzhambu.storage.application.query.ListStorageObjectsQuery"),
                 legacy(
                         "COMMAND_QUERY_RECORD:com.thundax.kuzhambu.storage.application.query.ListStorageReferenceOwnerTypesQuery"),
