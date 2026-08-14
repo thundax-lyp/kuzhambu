df6c4fac79e2dea9b9d203ed042c427591c1b286
Thundax <thundax@qq.com>
2026-08-14 13:07:57 +0800

    Refactor(knowledge): 删除旧图谱持久化模型

--- kuzhambu-servers/biz/knowledge/kuzhambu-knowledge-infra/src/main/java/com/thundax/kuzhambu/knowledge/infra/refinement/persistence/mapper/QualityReportMapper.java
+++
@@ -1,8 +0,0 @@
-package com.thundax.kuzhambu.knowledge.infra.refinement.persistence.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.thundax.kuzhambu.knowledge.infra.refinement.persistence.dataobject.QualityReportDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface QualityReportMapper extends BaseMapper<QualityReportDO> {}
